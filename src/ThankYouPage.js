b4e0362ff073067e567da80366ce4230478c8412
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2024-10-03 19:29:32 -0300

    mudança na pag de obrigada

--- src/ThankYouPage.js
+++ src/ThankYouPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './ThankYouPage.css'; // Se desejar adicionar estilos
 
-
 function ThankYouPage() {
     return (
         <div className="thank-you-container">
             <h1 className="text">Obrigado pelo contato</h1>
             <p className="paragrafo">Em breve entraremos em contato!</p>
             <a href="/" className="btn">Home</a>
+            <p className="credit">Desenvolvido por: Riquelme dos Santos Pimentel Carvalho Silva RU:4848809</p>
         </div>
     );
 }
